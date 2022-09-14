ff1b150a565fe430ffd2debb4b03ce1c03580f60
DinahWorld <dinath.sh@outlook.fr>
2022-09-14 23:34:52 +0200

    SpringBoot project starter (the real one)

--- src/main/java/io/javabrains/SecurityAdapter.java
+++ src/main/java/io/javabrains/SecurityAdapter.java
@@ -1,8 +1,9 @@
-package io.javabrains.inbox;
+package io.javabrains;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
