9b7496ff139b3b2b271aee8cad7d294af6b34915
DinahWorld <dinath.sh@outlook.fr>
2022-09-18 15:18:28 +0200

    Add Email view and fix navigation

--- src/main/java/io/javabrains/inbox/InboxApp.java
+++ src/main/java/io/javabrains/inbox/InboxApp.java
@@ -1,6 +1,8 @@
 package io.javabrains.inbox;
 
 import com.datastax.oss.driver.api.core.uuid.Uuids;
+import io.javabrains.inbox.email.Email;
+import io.javabrains.inbox.email.EmailRepository;
 import io.javabrains.inbox.inbox.emaillist.EmailListItem;
 import io.javabrains.inbox.inbox.emaillist.EmailListItemKey;
 import io.javabrains.inbox.inbox.emaillist.EmailListItemRepository;
@@ -15,47 +17,60 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
 import java.nio.file.Path;
-import java.util.Arrays;
-import java.util.UUID;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class InboxApp {
 
-	@Autowired FolderRepository folderRepository;
-	@Autowired
-	EmailListItemRepository emailListItemRepository;
-	public static void main(String[] args) {
-		SpringApplication.run(InboxApp.class, args);
-	}
+    @Autowired
+    FolderRepository folderRepository;
+    @Autowired
+    EmailListItemRepository emailListItemRepository;
+    @Autowired
+    EmailRepository emailRepository;
+
+    public static void main(String[] args) {
+        SpringApplication.run(InboxApp.class, args);
+    }
 
 
-	// Connect to Astra with creating DataStaxAstraProperties
-	@Bean
-	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
-			Path bundle = astraProperties.getSecureConnectBundle().toPath();
-		return builder -> builder.withCloudSecureConnectBundle(bundle);
-	}
-	@PostConstruct
-	public void init(){
-		folderRepository.save(new Folder("DinahWorld", "Inbox" , "blue"));
-		folderRepository.save(new Folder("DinahWorld", "Sent" , "green"));
-		folderRepository.save(new Folder("DinahWorld", "Important" , "yellow"));
+    // Connect to Astra with creating DataStaxAstraProperties
+    @Bean
+    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+        Path bundle = astraProperties.getSecureConnectBundle().toPath();
+        return builder -> builder.withCloudSecureConnectBundle(bundle);
+    }
 
-		for(int i = 0;i < 10; i++){
-			EmailListItemKey key = new EmailListItemKey();
-			key.setId("DinahWorld");
-			key.setLabel("Inbox");
-			key.setTimeUUID(Uuids.timeBased());
+    @PostConstruct
+    public void init() {
+        folderRepository.save(new Folder("DinahWorld", "Inbox", "blue"));
+        folderRepository.save(new Folder("DinahWorld", "Sent", "green"));
+        folderRepository.save(new Folder("DinahWorld", "Important", "yellow"));
 
-			EmailListItem item = new EmailListItem();
-			item.setKey(key);
-			item.setTo(Arrays.asList("DinahWorld"));
-			item.setSubject("Subject  " + i);
-			item.setUnread(true);
+        for (int i = 0; i < 10; i++) {
+            EmailListItemKey key = new EmailListItemKey();
+            key.setId("DinahWorld");
+            key.setLabel("Inbox");
+            key.setTimeUUID(Uuids.timeBased());
 
-			emailListItemRepository.save(item);
-		}
-	}
+            EmailListItem item = new EmailListItem();
+            item.setKey(key);
+            item.setTo(List.of("DinahWorld","Me","Someone"));
+            item.setSubject("Subject  " + i);
+            item.setUnread(true);
+
+            emailListItemRepository.save(item);
+
+            Email email = new Email();
+            email.setId(key.getTimeUUID());
+            email.setFrom("DinahWorld");
+            email.setSubject(item.getSubject());
+            email.setBody("Body " + i);
+            email.setTo(item.getTo());
+
+            emailRepository.save(email);
+        }
+    }
 
 }
--- src/main/java/io/javabrains/inbox/controllers/EmailViewController.java
+++ src/main/java/io/javabrains/inbox/controllers/EmailViewController.java
@@ -1,12 +1,10 @@
 package io.javabrains.inbox.controllers;
 
-import com.datastax.oss.driver.api.core.uuid.Uuids;
-import io.javabrains.inbox.inbox.emaillist.EmailListItem;
-import io.javabrains.inbox.inbox.emaillist.EmailListItemRepository;
+import io.javabrains.inbox.email.Email;
+import io.javabrains.inbox.email.EmailRepository;
 import io.javabrains.inbox.inbox.folder.Folder;
 import io.javabrains.inbox.inbox.folder.FolderRepository;
 import io.javabrains.inbox.inbox.folder.FolderService;
-import org.ocpsoft.prettytime.PrettyTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -14,59 +12,51 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
-import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-// Will detect if the user is logged or not
 @Controller
-public class InboxController {
-
+public class EmailViewController {
     @Autowired
     private FolderRepository folderRepository;
     @Autowired
     private FolderService folderService;
     @Autowired
-    private EmailListItemRepository emailListItemRepository;
+    private EmailRepository emailRepository;
 
-    @GetMapping(value = "/")
-    public String homePage(@RequestParam(required = false) String folder, @AuthenticationPrincipal OAuth2User principal, Model model) {
+    @GetMapping(value = "/email/{id}")
+    public String emailView(@PathVariable UUID id, @AuthenticationPrincipal OAuth2User principal, Model model) {
         //Check if the user  is looged
         if (principal == null || !StringUtils.hasText(principal.getAttribute("login"))) {
             return "index";
         }
 
         // Fetch folder
-        //get the id
+        //get the idt
         String userId = principal.getAttribute("login");
         //get the correct folder by user
         List<Folder> userFolders = folderRepository.findAllById(userId);
         model.addAttribute("userFolders", userFolders);
-
         List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
         model.addAttribute("defaultFolders", defaultFolders);
 
-        // Fetch Messages
-        if(!StringUtils.hasText(folder)){
-            folder = "inbox";
+        Optional<Email> optionalEmail = emailRepository.findById(id);
+
+        if (!optionalEmail.isPresent()) {
+            return "inbox-page";
         }
 
-        List<EmailListItem> emailList = emailListItemRepository.findAllByKey_IdAndKey_Label(userId, folder);
+        Email email = optionalEmail.get();
+        String toIds = String.join(", ",email.getTo());
+        model.addAttribute("email", email);
+        model.addAttribute("toIds", email);
 
-        PrettyTime p = new PrettyTime();
-        emailList.stream().forEach(emailItem -> {
-            UUID timeUuid = emailItem.getKey().getTimeUUID();
-            Date emailDateTime = new Date(Uuids.unixTimestamp(timeUuid));
-            //give the "2 ago" "2 hours ago"
-            emailItem.setAgoTimeString(p.format(emailDateTime));
+        return "email-page";
 
-        });
-
-        model.addAttribute("emailList", emailList);
-        model.addAttribute("folderName",folder);
-
-        return "inbox-page";
     }
+
 }
