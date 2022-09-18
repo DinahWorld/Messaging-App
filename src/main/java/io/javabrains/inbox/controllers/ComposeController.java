1700590b217486c4dd2aa95abcf7ae23b9c97227
DinahWorld <dinath.sh@outlook.fr>
2022-09-19 00:06:07 +0200

    Add Reply

--- src/main/java/io/javabrains/inbox/controllers/ComposeController.java
+++ src/main/java/io/javabrains/inbox/controllers/ComposeController.java
@@ -1,11 +1,8 @@
 package io.javabrains.inbox.controllers;
 
-import io.javabrains.inbox.email.Email;
-import io.javabrains.inbox.email.EmailRepository;
 import io.javabrains.inbox.inbox.folder.Folder;
 import io.javabrains.inbox.inbox.folder.FolderRepository;
 import io.javabrains.inbox.inbox.folder.FolderService;
-import org.apache.tomcat.util.log.SystemLogHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -14,56 +11,49 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Controller
-public class EmailViewController {
+public class ComposeController {
     @Autowired
     private FolderRepository folderRepository;
     @Autowired
     private FolderService folderService;
-    @Autowired
-    private EmailRepository emailRepository;
 
-    @GetMapping(value = "/email/{id}")
-    public String emailView(@PathVariable UUID id, @AuthenticationPrincipal OAuth2User principal, Model model) {
+
+    @GetMapping(value = "/compose")
+    public String getComposePage(@RequestParam(required = false) String to, @AuthenticationPrincipal OAuth2User principal, Model model) {
         //Check if the user  is looged
         if (principal == null || !StringUtils.hasText(principal.getAttribute("login"))) {
             return "index";
         }
 
         // Fetch folder
-        //get the idt
+        //get the id
         String userId = principal.getAttribute("login");
         //get the correct folder by user
         List<Folder> userFolders = folderRepository.findAllById(userId);
         model.addAttribute("userFolders", userFolders);
+
         List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
         model.addAttribute("defaultFolders", defaultFolders);
 
-        Optional<Email> optionalEmail = emailRepository.findById(id);
+        if (StringUtils.hasText(to)) {
+            String[] splitIds = to.split(",");
+            List<String> uniqueToIds = Arrays.asList(splitIds)
+                    .stream()
+                    .map(id -> StringUtils.trimWhitespace(id))
+                    .filter(id -> StringUtils.hasText(id))
+                    .distinct()
+                    .collect(Collectors.toList());
 
-        if (!optionalEmail.isPresent()) {
-            return "inbox-page";
+            model.addAttribute("toIds", String.join(", ", uniqueToIds));
         }
 
-        Email email = optionalEmail.get();
-        String toIds = email
-                .getTo()
-                .toString()
-                .replace("[", "")
-                .replace("]", "");
-
-
-        model.addAttribute("email", email);
-        model.addAttribute("toIds", toIds);
-
-        return "email-page";
-
+        return "compose-page";
     }
-
 }
